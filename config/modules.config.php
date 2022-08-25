f9448401084a9a03d81494a1b585360173136f63
febriramandha <febri.ramandha@gmail.com>
2022-08-25 14:55:35 +0700

    Blog Module

--- config/modules.config.php
+++ config/modules.config.php
@@ -18,4 +18,5 @@ return [
     'Laminas\Validator',
     'Application',
     'Album', // <-- Add this line
+    'Blog',
 ];
---
+++ module/Blog/src/Factory/LaminasDbSqlRepositoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Blog\Factory;
+
+use Interop\Container\ContainerInterface;
+use Blog\Model\Post;
+use Blog\Model\LaminasDbSqlRepository;
+use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Hydrator\ReflectionHydrator;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+
+class LaminasDbSqlRepositoryFactory implements FactoryInterface
+{
+    /**
+     * @param ContainerInterface $container
+     * @param string $requestedName
+     * @param null|array $options
+     * @return LaminasDbSqlRepository
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        return new LaminasDbSqlRepository(
+            $container->get(AdapterInterface::class),
+            new ReflectionHydrator(),
+            new Post('', '')
+        );
+    }
+}
